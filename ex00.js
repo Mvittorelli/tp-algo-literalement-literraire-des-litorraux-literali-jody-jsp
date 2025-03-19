7ec1f6f01e0a79b4e2f04d55a8953a6e844ee706
Mvittorelli <mvittorelli@edenschool.fr>
2025-03-19 10:11:33 +0100

    jody

---
+++ ex00.js
@@ -0,0 +1,7 @@
+function creerPersonne(nom, age, ville) {
+    const newPerson = { nom: nom, age: age, ville: ville }; 
+    return newPerson; 
+}
+
+console.log(creerPersonne("Alice", 30, "Paris")); // { nom: 'Alice', age: 30, ville: 'Paris' }
+console.log(creerPersonne("Bob", 25, "Lyon")); // { nom: 'Bob', age: 25, ville: 'Lyon' }
