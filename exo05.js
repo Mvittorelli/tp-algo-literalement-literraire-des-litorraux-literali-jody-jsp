486f86856c75cd656a62cff23f6d2b4c95737e08
Mvittorelli <mvittorelli@edenschool.fr>
2025-03-19 11:00:26 +0100

    jody

---
+++ exo05.js
@@ -0,0 +1,16 @@
+function listerPersonnes(personnes) {
+    let result = "";
+	for (let i = 0; i < personnes.length; i++) {
+        result += `${personnes[i].nom} : ${personnes[i].age} ans`
+        if (i < personnes.length - 1) result += ", "
+    }
+    return result + "."
+}
+
+const personnes = [
+	{ nom: "Alice", age: 30 },
+	{ nom: "Bob", age: 25 },
+	{ nom: "Charlie", age: 35 }
+];
+
+console.log(listerPersonnes(personnes)); // 'Alice : 30 ans, Bob : 25 ans, Charlie : 35 ans.'
